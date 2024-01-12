ffd53c62d95482014782aa879d705f9af80f66e0
AShvab <shvabovska@gmail.com>
2024-01-12 23:19:03 +0200

    add cart

--- 01-mini-shop/assets/scripts/app.js
+++ 01-mini-shop/assets/scripts/app.js
@@ -15,6 +15,22 @@ class Product {
 class ShoppingCart {
   items = [];
 
+  set cartItems(value){
+    this.items = value;
+    this.totalOutput.innerHTML = ` <h2>Total: \$${this.totalAmount.toFixed(2)} </h2>`;
+  }
+
+  get totalAmount() {
+    const sum = this.items.reduce((prevValue, currentItem) => prevValue + currentItem.price, 0);
+    return sum;
+  }
+
+  addProduct(product) {
+   const updatedItems = [...this.items]  
+   updatedItems.push(product); 
+   this.cartItems = updatedItems; 
+  }
+
   render() {
     const cartEl = document.createElement("section");
     cartEl.innerHTML = `
@@ -22,6 +38,7 @@ class ShoppingCart {
     <button>Order Now!</button>
     `;
     cartEl.className = "cart";
+    this.totalOutput = cartEl.querySelector("h2");
     return cartEl;
   }
 }
@@ -31,7 +48,8 @@ class ProductItem {
     this.product = product;
   }
   addToCart() {
-    console.log(this.product);
+    // console.log(this.product);
+    App.addProductToCart(this.product);
   }
   render() {
     const prodEl = document.createElement("li");
@@ -131,8 +149,8 @@ class ProductList {
 class Shop {
   render() {
     const renderHook = document.getElementById("app");
-    const cart = new ShoppingCart();
-    const cartEl = cart.render();
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
 
     const productList = new ProductList();
     const prodListEl = productList.render();
@@ -142,5 +160,17 @@ class Shop {
   }
 }
 
-const shop = new Shop();
-shop.render();
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
