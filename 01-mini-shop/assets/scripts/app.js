55895dc8d92f573479ba7792bd0f9c69de2b1dd9
AShvab <shvabovska@gmail.com>
2024-01-14 23:29:54 +0200

    added cart logic

--- 01-mini-shop/assets/scripts/app.js
+++ 01-mini-shop/assets/scripts/app.js
@@ -20,9 +20,14 @@ class ElementAttribute {
 }
 
 class Component {
-  constructor(renderHookId) {
+  constructor(renderHookId, shouldRender = true) {
     this.hookId = renderHookId;
+    if (shouldRender) {
+      this.render();
+    }
   }
+
+  // render() {}
 
   createRootElement(tag, cssClasses, attributes) {
     const rootElement = document.createElement(tag);
@@ -39,7 +44,7 @@ class Component {
   }
 }
 
-class ShoppingCart extends Component{
+class ShoppingCart extends Component {
   items = [];
 
   set cartItems(value) {
@@ -57,10 +62,14 @@ class ShoppingCart extends Component{
     return sum;
   }
 
-constructor(renderHookId){
-  super(renderHookId);
-}
-
+  constructor(renderHookId) {
+    super(renderHookId, false);
+    this.orderProducts = () =>{
+      console.log("order");
+      console.log(this.items);
+    }
+    this.render()
+  }
 
   addProduct(product) {
     const updatedItems = [...this.items];
@@ -68,29 +77,39 @@ constructor(renderHookId){
     this.cartItems = updatedItems;
   }
 
+  // orderProducts() {
+  //   console.log("order");
+  //   console.log(this.items);
+  // }
+
   render() {
-    const cartEl = this.createRootElement('section', 'cart')
+    const cartEl = this.createRootElement("section", "cart");
     cartEl.innerHTML = `
     <h2>Total: \$${0} </h2>
     <button>Order Now!</button>
     `;
+    const orderButton = cartEl.querySelector("button");
 
+    // orderButton.addEventListener("click", () => this.orderProducts());
+    // orderButton.addEventListener("click",  this.orderProducts.bind(this));
+    orderButton.addEventListener("click",  this.orderProducts);
     this.totalOutput = cartEl.querySelector("h2");
-
   }
 }
 
-class ProductItem {
-  constructor(product) {
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId, false);
     this.product = product;
+    this.render();
   }
   addToCart() {
     // console.log(this.product);
     App.addProductToCart(this.product);
   }
   render() {
-    const prodEl = document.createElement("li");
-    prodEl.className = "product-item";
+    const prodEl = this.createRootElement("li", "product-item");
+
     prodEl.innerHTML = `
       <div>
       <img src='${this.product.imageUrl}' alt="${this.product.title}">
@@ -104,96 +123,102 @@ class ProductItem {
       `;
     const addCartButton = prodEl.querySelector("button");
     addCartButton.addEventListener("click", this.addToCart.bind(this));
-    return prodEl;
   }
 }
 
-class ProductList {
-  products = [
-    new Product(
-      "A Book",
-      "https://cdn.pixabay.com/photo/2017/02/26/21/39/rose-2101475_1280.jpg",
-      9.99,
-      "A Book"
-    ),
+class ProductList extends Component {
+  products = [];
+  constructor(renderHookId) {
+    super(renderHookId);
+    this.fetchProducts();
+  }
 
-    new Product(
-      "A Cup",
-      "https://cdn.pixabay.com/photo/2020/02/15/05/15/cup-of-coffee-4850060_1280.jpg",
-      1.99,
-      "A Cup"
-    ),
+  fetchProducts() {
+    this.products = [
+      new Product(
+        "A Book",
+        "https://cdn.pixabay.com/photo/2017/02/26/21/39/rose-2101475_1280.jpg",
+        9.99,
+        "A Book"
+      ),
 
-    new Product(
-      "A Pillow",
-      "https://cdn.pixabay.com/photo/2016/10/12/22/07/break-1736072_1280.jpg",
-      19.99,
-      "A soft pillow"
-    ),
+      new Product(
+        "A Cup",
+        "https://cdn.pixabay.com/photo/2020/02/15/05/15/cup-of-coffee-4850060_1280.jpg",
+        1.99,
+        "A Cup"
+      ),
 
-    new Product(
-      "A Carpet",
-      "https://cdn.pixabay.com/photo/2012/11/07/17/03/carpet-65100_1280.jpg",
-      119.99,
-      "A carpet which you might like - or not"
-    ),
+      new Product(
+        "A Pillow",
+        "https://cdn.pixabay.com/photo/2016/10/12/22/07/break-1736072_1280.jpg",
+        19.99,
+        "A soft pillow"
+      ),
 
-    new Product(
-      "A Blanket",
-      "https://cdn.pixabay.com/photo/2021/04/09/18/01/cat-6165131_1280.jpg",
-      29.99,
-      "A blanket just for you!"
-    ),
+      new Product(
+        "A Carpet",
+        "https://cdn.pixabay.com/photo/2012/11/07/17/03/carpet-65100_1280.jpg",
+        119.99,
+        "A carpet which you might like - or not"
+      ),
 
-    // {
-    //   title: "A Pillow",
-    //   imageUrl:
-    //     "https://cdn.pixabay.com/photo/2016/10/12/22/07/break-1736072_1280.jpg",
-    //   price: 19.99,
-    //   description: "A soft pillow",
-    // },
-    // {
-    //   title: "A Carpet",
-    //   imageUrl:
-    //     "https://cdn.pixabay.com/photo/2012/11/07/17/03/carpet-65100_1280.jpg",
-    //   price: 119.99,
-    //   description: "A carpet which you might like - or not",
-    // },
-    // {
-    //   title: "A Blanket",
-    //   imageUrl:
-    //     "https://cdn.pixabay.com/photo/2021/04/09/18/01/cat-6165131_1280.jpg",
-    //   price: 29.99,
-    //   description: "A blanket just for you!",
-    // },
-  ];
-  constructor() {}
+      new Product(
+        "A Blanket",
+        "https://cdn.pixabay.com/photo/2021/04/09/18/01/cat-6165131_1280.jpg",
+        29.99,
+        "A blanket just for you!"
+      ),
+
+      // {
+      //   title: "A Pillow",
+      //   imageUrl:
+      //     "https://cdn.pixabay.com/photo/2016/10/12/22/07/break-1736072_1280.jpg",
+      //   price: 19.99,
+      //   description: "A soft pillow",
+      // },
+      // {
+      //   title: "A Carpet",
+      //   imageUrl:
+      //     "https://cdn.pixabay.com/photo/2012/11/07/17/03/carpet-65100_1280.jpg",
+      //   price: 119.99,
+      //   description: "A carpet which you might like - or not",
+      // },
+      // {
+      //   title: "A Blanket",
+      //   imageUrl:
+      //     "https://cdn.pixabay.com/photo/2021/04/09/18/01/cat-6165131_1280.jpg",
+      //   price: 29.99,
+      //   description: "A blanket just for you!",
+      // },
+    ];
+    this.renderProducts();
+  }
+
+  renderProducts() {
+    for (const prod of this.products) {
+      new ProductItem(prod, "prod-list");
+    }
+  }
 
   render() {
-    const renderHook = document.getElementById("app");
-    const prodList = document.createElement("ul");
-    prodList.className = "product-list";
-
-    for (const prod of this.products) {
-      const productItem = new ProductItem(prod);
-      const prodEl = productItem.render();
-      prodList.append(prodEl);
+    this.createRootElement("ul", "product-list", [
+      new ElementAttribute("id", "prod-list"),
+    ]);
+    if (this.products && this.products.length > 0) {
+      this.renderProducts();
     }
-    return prodList;
   }
 }
 
 class Shop {
+  constructor() {
+    this.render();
+  }
+
   render() {
-    const renderHook = document.getElementById("app");
-    this.cart = new ShoppingCart('app');
-    this.cart.render();
-
-    const productList = new ProductList();
-    const prodListEl = productList.render();
-
-  
-    renderHook.append(prodListEl);
+    this.cart = new ShoppingCart("app");
+    new ProductList("app");
   }
 }
 
@@ -202,7 +227,6 @@ class App {
 
   static init() {
     const shop = new Shop();
-    shop.render();
     this.cart = shop.cart;
   }
   static addProductToCart(product) {
