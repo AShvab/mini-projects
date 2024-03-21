bfcf4388a9bd9b2e4e3bf53deef724d50dfd86e3
AShvab <shvabovska@gmail.com>
2024-03-21 23:53:14 +0200

    recursion

--- 05-advanced-function-concepts/app.js
+++ 05-advanced-function-concepts/app.js
@@ -25,20 +25,72 @@ function printHobbies(h) {
   h.push("new hobby");
   console.log(h);
 }
-printHobbies(hobbies)
-
-
+printHobbies(hobbies);
 
 // factory functions:
-function createTaxCalculator(tax){
-    function calculateTax(amount) {
-        return amount * tax;
-      }
-      return calculateTax
+
+let multiplier = 1.1;
+function createTaxCalculator(tax) {
+  function calculateTax(amount) {
+    console.log(multiplier);
+    return amount * tax * multiplier;
+  }
+  return calculateTax;
 }
 
-const calculateVatAmount = createTaxCalculator(0.19)
-const calculateIncomeTaxAmount = createTaxCalculator(0.25)
+const calculateVatAmount = createTaxCalculator(0.19);
+const calculateIncomeTaxAmount = createTaxCalculator(0.25);
 
-console.log(calculateVatAmount(100))
-console.log(calculateVatAmount(200))
+// function calculateTaxCalculator will take latest multiplier,because it is global
+multiplier = 1.2;
+
+console.log(calculateVatAmount(100));
+console.log(calculateVatAmount(200));
+
+let userName = "Anna";
+function greetUser() {
+  let name = "Max";
+  console.log("Hi " + name);
+}
+let name = "Maximilian";
+userName = "John";
+greetUser();
+
+// recursion
+// function powerOf(value, power) {
+//   let result = 1
+
+//   for(let i = 0; i < power; i++) {
+// result *= value
+//   }
+//   return result
+// }
+
+// console.log(powerOf(2, 3)); // 2*2*2
+
+function powerOf(x, n) {
+  // if(n===1){
+  //   return x
+  // }
+  // return x * powerOf(x, n - 1);
+  return n===1 ? x : powerOf(x, n - 1);
+}
+console.log(powerOf(2, 3));
+
+const myself ={
+  name: "Anna",
+  friends:[
+    {
+      name: "Manuel",
+      friends:[
+        {
+          name:"John",
+        }
+      ]
+    },
+    {
+      name: "Julia",
+      
+    }
+  ]
+}
